8690a668880da0826258b27618471b9efb23cc09
JulianGonzalez13 <juldgonzalezc@udistrital.edu.co>
2024-08-25 17:12:52 -0500

    docker sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "0 0 * * * certbot renew --quiet --post-hook 'apache2ctl graceful'" | cront
 service cron start
 
 if [ ! -f /etc/letsencrypt/live/tu-dominio.com/fullchain.pem ]; then
-  certbot certonly --apache --non-interactive --agree-tos -m practicampo@udistrital.edu.co -d practicampo.udistrital.edu.co
+  certbot certonly --webroot -w /var/www/html/practicampo --non-interactive --agree-tos -m practicampo@udistrital.edu.co -d practicampo.udistrital.edu.co
 fi
 
 # Iniciar Apache en modo foreground
