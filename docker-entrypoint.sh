c3398926cdf4d8f6ce5bb59b8efe64596355ab0b
PractiCampo <practicampo@udistrital.edu.co>
2024-08-27 10:56:10 +0000

    https funcionando

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ echo "0 0 * * * certbot renew --quiet --post-hook 'apache2ctl graceful'" | cront
 
 service cron start
 
-if [ ! -f /etc/letsencrypt/live/tu-dominio.com/fullchain.pem ]; then
+if [ ! -f /etc/letsencrypt/live/practicampo.udistrital.edu.co/fullchain.pem ]; then
   certbot certonly --webroot -w /var/www/html/practicampo/public --non-interactive --agree-tos -m practicampo@udistrital.edu.co -d practicampo.udistrital.edu.co
 fi
 
