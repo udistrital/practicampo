1bfa079e5598ed657652fee32e89d59aba37ee11
JulianGonzalez13 <juldgonzalezc@udistrital.edu.co>
2024-10-17 12:46:38 -0500

    viaticos posgrado

--- app/Http/Controllers/Solicitud/SolicitudController.php
+++ app/Http/Controllers/Solicitud/SolicitudController.php
@@ -1480,7 +1480,9 @@ class SolicitudController extends Controller
         $vlr_docen_max= $sistema->vlr_docen_max;
         $vlr_docen_min= $sistema->vlr_docen_min;
 		
-		$practicas_integradas = practicas_integradas::where('id','=',$id)->first();		
+		$practicas_integradas = practicas_integradas::where('id','=',$id)->first();
+        $programa_academico = DB::table('programa_academico')
+        ->where('id',$proyeccion_preliminar->id_programa_academico)->first();	
 
         if(Auth::user()->id_role == 1 ||  Auth::user()->id_role == 4 || Auth::user()->id_role == 5)
         {
@@ -1648,7 +1650,10 @@ class SolicitudController extends Controller
                                 $viaticos_estudiantes_rp = $num_estudiantes*$vlr_estud_max*$num_dias_rp;
                                 $viaticos_docente_rp = ($num_dias_rp-0.5)*$vlr_docen_max*$total_docentes;
                             }
-                                        
+                            if($programa_academico->pregado == 0){
+								$viaticos_estudiantes_rp=0;
+							}        
+
                             $costo_total_transporte_menor_rp=$costos_proyeccion->costo_total_transporte_menor_rp;
                             $valor_estimado_transporte_rp=$costos_proyeccion->valor_estimado_transporte_rp;
                         
@@ -1772,12 +1777,15 @@ class SolicitudController extends Controller
                                 $viaticos_estudiantes_ra = $num_estudiantes*$vlr_estud_max*$num_dias_ra;
                                 $viaticos_docente_ra = ($num_dias_ra-0.5)*$vlr_docen_max*$total_docentes;
                             }
+                            if($programa_academico->pregado == 0){
+								$viaticos_estudiantes_ra=0;
+							}  
                             
                             $costo_total_transporte_menor_ra=$costos_proyeccion->costo_total_transporte_menor_ra;
                             $valor_estimado_transporte_ra=$costos_proyeccion->valor_estimado_transporte_ra;
-
                             
                             $nuevo_costo_total_transporte_menor_ra = $vlr_trans_menor_ra_1 + $vlr_trans_menor_ra_2 + $vlr_trans_menor_ra_3 + $vlr_trans_menor_ra_4;
+                            
                             $total_presupuesto_sin_transporte_menor_ra = $costos_proyeccion->total_presupuesto_ra - $costo_total_transporte_menor_ra; 
 
                             $costos_proyeccion->costo_total_transporte_menor_ra = $nuevo_costo_total_transporte_menor_ra;
