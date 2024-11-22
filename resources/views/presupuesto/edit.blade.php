fbd0af26241e5e791a9087ba8ca3f278f0c420d5
JulianGonzalez13 <juldgonzalezc@udistrital.edu.co>
2024-11-21 20:58:41 -0500

    Ajuste asignación presupuestos

--- resources/views/presupuesto/edit.blade.php
+++ resources/views/presupuesto/edit.blade.php
@@ -50,19 +50,19 @@
                                 }
                             @endphp
                             @if ($tipo_actual !== $tipo_anterior)
-                                <hr class="divider">
+                                <hr class="divider border-primary">
                                 <h4 class="">{{ $tipo_actual }}</h4>
-                                <hr class="divider">
+                                <hr class="divider border-primary">
                                 @php
                                     $tipo_anterior = $tipo_actual;
                                 @endphp
                             @endif
-                            <div class="card">
+                            <div class="card border-secondary">
                                 <div class="form-group row ml-1">
                                     <div class="col-md-8">
                                     <label for="presupuesto" class="col-form-label text-md-left col-md-12" title=""><i class="" 
                                         data-toggle="tooltip" data-placement="left" 
-                                        data-title="" style="font-size: 0.813rem"></i>Presupuesto {{ $pa->programa_academico }}</label>
+                                        data-title="" style="font-size: 0.813rem"></i>Presupuesto dado a {{ $pa->programa_academico }}</label>
                                         <input id="presupuesto" type="text" class="form-control @error('vlr_docen_min') is-invalid @enderror col-md-12" name="presupuesto" 
                                         value="$ {{number_format($presupuesto,'0',',','.')}}" autocomplete="off" autofocus title="" disabled>
                                     </div> 
