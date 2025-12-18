53637a661b5b7fc4784ace8ffa62f43f05606bf3
JulianGonzalez13 <juldgonzalezc@udistrital.edu.co>
2025-12-18 11:56:33 -0500

    bash para backups automaticos

--- backup.sh
+++ backup.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
-
+source /home/decpracti/practicampo/.env
 CONTAINER_NAME=dbpracticampo
 DB_USER=viverouser
 DB_NAME=udpracticampo
 BACKUP_DIR=/home/decpracti/practicampo/db_backup/
 
 # Fechas en formato YYYY-MM-DD
-CURRENT_DATE=$(date +\%F)
-CURRENT_MONTH=$(date +\%Y-\%m)
-LAST_MONTH=$(date -d "last month" +\%Y-\%m)
+#CURRENT_DATE=$(date +%F)
+#CURRENT_HOUR=$(date +%F_%H)
 
 # Rutas backup
 DAILY_BACKUP_FILE="$BACKUP_DIR/daily_backup.sql"
-MONTHLY_BACKUP_FILE="$BACKUP_DIR/backup_$LAST_MONTH.sql"
+HOURLY_BACKUP_FILE="$BACKUP_DIR/hourly_backup.sql"
 
-# Condicional para guardar un backup mensual
-if [ $(date +\%d) -eq 01 ]; then
-    cp $DAILY_BACKUP_FILE $MONTHLY_BACKUP_FILE
+mkdir -p "$BACKUP_DIR"
+
+# Validar variable de entorno
+if [ -z "$DB_PASSWORD" ]; then
+  echo "ERROR: DB_PASSWORD no está definida" >&2
+  exit 1
 fi
 
 # Se ejecuta el comando en el contenedor de docker
-docker exec $CONTAINER_NAME /usr/bin/mysqldump -u $DB_USER --password=DBvivUSER.2021$ $DB_NAME > $DAILY_BACKUP_FILE
+docker exec "$CONTAINER_NAME" /usr/bin/mysqldump -u "$DB_USER" --password="$DB_PASSWORD" "$DB_NAME" > "$HOURLY_BACKUP_FILE"
 
-# Parte por validar que funcione: crontab para que se ejecute todos los dias a x hora
+# Condicional para guardar un backup diario
+if [ ! -f "$DAILY_BACKUP_FILE" ] || \
+   [ "$(date -d "$(stat -c %y "$DAILY_BACKUP_FILE")" +%F)" != "$(date +%F)" ]; then
+
+    docker exec "$CONTAINER_NAME" /usr/bin/mysqldump -u "$DB_USER" --password="$DB_PASSWORD" "$DB_NAME" > "$DAILY_BACKUP_FILE"
+fi
+
+# Parte por validar que funcione: crontab para que se ejecute todas las horas
+# chmod +x /home/decpracti/practicampo/backup.sh
+# systemctl status cron
+    # (si no está activo)
+    # sudo systemctl enable --now cron
 # crontab -e
-# 0 3 * * * /home/decpracti/practicampo/backup.sh
+    # 0 * * * * /home/decpracti/practicampo/backup.sh
+# crontab -l
